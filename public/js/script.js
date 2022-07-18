683cc89fff56651b60533daf3f301477ac4eb8e4
msimbao <32774483+msimbao@users.noreply.github.com>
2022-07-17 22:54:44 -0500

    Please just push ...

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,55 @@
-var id=806;
-let urlString = window.location.href;
-let paramString = urlString.split('?')[1];
-let queryString = new URLSearchParams(paramString);
-for(let pair of queryString.entries()) {
-    console.log("Key is:" + pair[0]);
-    console.log("Value is:" + pair[1]);
-    id = pair[1] 
-}
-console.log(id);
+  
+        var id=866;
+        let urlString = window.location.href;
+        let paramString = urlString.split('?')[1];
+        let queryString = new URLSearchParams(paramString);
+        for(let pair of queryString.entries()) {
+            console.log("Key is:" + pair[0]);
+            console.log("Value is:" + pair[1]);
+            id = pair[1] 
+        }
+        console.log(id);
 
-$.post( "/test?id=" + id, function( data ) {
-$( ".id" ).html( data.id );
-$( ".name" ).html( data.name );
-$( ".storage" ).html( data.storage.name );
-$( ".weight" ).html( data.weight + ' ' + data.weight_unit.name );
-});
+        $.post( "/read?id=" + id, function( data ) {
+    $( "#id" ).html( data.id );
+    $( "#name" ).html( data.name );
+    $( "#storage" ).html( data.storage.name );
+    $( "#weight" ).html( data.weight + ' ' + data.units );
+  });
 
+  function makePUTrequest() {
+
+        var currentWeight = document.getElementById("weight").innerHTML
+        unit = currentWeight.split(" ")[1]
+        currentWeight = currentWeight.split(" ")[0]  
+        console.log("currentWeight is : " +  currentWeight)
+        // console.log(currentWeight);
+        var e = document.getElementById("addOrSubtract");
+        var addOrSubtract = e.value;
+
+        var amount = document.getElementById("amount").value;
+
+        if (addOrSubtract == 0){
+            var leftOverAmount = parseFloat(currentWeight) - parseFloat(amount);
+            console.log("leftOverAmount is : " + leftOverAmount)
+        }else{
+            var leftOverAmount = parseFloat(currentWeight) + parseFloat(amount);
+            console.log("currentWeight is : " + currentWeight)
+            console.log("amount is : " + amount)
+            console.log("leftOverAmount is : " + leftOverAmount)
+        }
+        
+        $.ajax({
+            url: '/write?id=' + id + '&amount=' + leftOverAmount,
+            type: 'PUT',
+            success: function (result) {
+                console.log(result)
+                swal("Done", "For this stock, there are " + leftOverAmount + " " + unit + " left", "success",{
+                    button: "Aww yeaaahhh!",
+                  })
+                .then((value) => {
+                    location.reload();
+                  });;
+            }
+        });
+    }
